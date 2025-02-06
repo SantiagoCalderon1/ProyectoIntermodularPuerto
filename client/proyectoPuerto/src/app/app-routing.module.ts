fb2587f8ad907cf3083433c9ae39959d322f6521
Dami21011 <damrodlop@alu.edu.gva.es>
2025-02-06 18:11:41 +0100

    BackLog Terminado sin diseño

--- client/proyectoPuerto/src/app/app-routing.module.ts
+++ client/proyectoPuerto/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaComponent } from './roles/components/lista/lista.component';
+import { FormularioComponent } from './roles/components/formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'roles', component: ListaComponent },
+  { path: 'formulario/:id', component: FormularioComponent },
+  { path: '', redirectTo: 'roles', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ server/app/controller/controllerRoles.php
@@ -0,0 +1,35 @@
+<?php
+    include '../model/roles.php';
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+    $uri_parts = explode('/', $_SERVER['REQUEST_URI']); // Separamos la URI en partes
+    $lastpart = $uri_parts[count($uri_parts) - 1]; // Obtenemos el último elemento de la URI
+    $input = json_decode(file_get_contents('php://input'), true); // Obtenemos los datos del cuerpo de la petición en formato JSON
+
+    switch($_SERVER['REQUEST_METHOD']){
+        case 'GET':
+            if(is_numeric($lastpart)){
+                echo json_encode(Roles::getAllRoles($lastpart));
+            } else {
+                echo json_encode(Roles::getAllRoles());
+            }
+            break;
+        case 'PUT':
+            $respuesta = Roles::modificarRol($lastpart, $input['nombre_rol'], $input['descripcion']);
+            if ($respuesta) {
+                echo json_encode(
+                    ['success' => true, 
+                    'message' => 'Rol actualizado con éxito'] 
+                );
+            } else {
+                echo json_encode(
+                    ['success' => false, 
+                    'message' => 'No se encontró la id del rol'] 
+                );
+            }
+            break;
+    }
+?>
