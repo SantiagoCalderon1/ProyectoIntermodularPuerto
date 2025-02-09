6cacb884c37f1b8ef81a99fc592bcafdf0e67519
Santiago Calderon <santiago.contactoyt@gmail.com>
2025-02-09 04:37:44 +0100

    uniendo ramas

--- client/proyectoPuerto/src/app/app-routing.module.ts
+++ client/proyectoPuerto/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InstalacionComponent } from './instalacion/components/creacion/instalacion.component';
-import { ListaComponent } from './instalacion/components/lista/lista.component';
+import { ListaComponent as instalacionListaComponent } from './instalacion/components/lista/lista.component';
 
 const routes: Routes = [
   //meter rutas
   { path: 'creacion', component: InstalacionComponent },
   { path: 'creacion/:id_instalacion/:tipo', component: InstalacionComponent },
-  { path: 'listado', component: ListaComponent },
+  { path: 'listado', component: instalacionListaComponent },
   { path: '', redirectTo: 'listado', pathMatch: 'full' }
 ];
 
